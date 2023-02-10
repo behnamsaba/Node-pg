8b23f4d13f337a05c72bea02664aeea46f06e4bf
behnamsaba <behnams71@gmail.com>
2023-02-10 15:31:55 -0800

    put updated

--- routes/invoices.js
+++ routes/invoices.js
@@ -39,12 +39,30 @@ router.post('/', async (req,res,next) => {
 
 router.put('/:id', async (req,res,next) => {
     try{
-        const { amt } = req.body;
-        const result = await db.query('UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *',[amt,req.params.id]);
-        if(result.rows.length === 0){
+        const { amt , paid } = req.body;
+        let paidDate = null;
+        const findData = await db.query('SELECT * FROM invoices WHERE id=$1',[req.params.id]);
+        if(findData.rows.length === 0){
             throw new ExpressError('Not Found', 404);
         }
-        return res.json({invoice: result.rows[0] });
+        const currPaidDate = findData.rows[0].paid_date;
+
+        if (!currPaidDate && paid) {
+          paidDate = new Date();
+        } else if (!paid) {
+          paidDate = null
+        } else {
+          paidDate = currPaidDate;
+        }
+    
+        const result = await db.query(
+              `UPDATE invoices
+               SET amt=$1, paid=$2, paid_date=$3
+               WHERE id=$4
+               RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+            [amt, paid, paidDate, id]);
+    
+        return res.json({"invoice": result.rows[0]});
     }catch(e){
         return next(e);
     }
